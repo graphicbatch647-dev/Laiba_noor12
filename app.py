4a100a18a4f963e84b062e0e3806bede0f5dbd31
Laibanoor12. <laibanoor4332@gmail.com>
2026-04-13 04:52:17 -0700

    nip

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 from flask import Flask, render_template, request
+import requests
+import os
 
 app = Flask(__name__)
 
-# Home page
-@app.route('/')
-def home():
-    return render_template('index.html')
+API_KEY = "aapki_api_key_yahan"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
-# Greet page
-@app.route('/greet', methods=['GET', 'POST'])
-def greet():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        return render_template('greet.html', name=name)
-    return render_template('index.html')
+@app.route("/", methods=["GET", "POST"])
+def index():
+    weather = None
+    error = None
+    if request.method == "POST":
+        city = request.form.get("city")
+        response = requests.get(BASE_URL, params={
+            "q": city,
+            "appid": API_KEY,
+            "units": "metric"
+        })
+        if response.status_code == 200:
+            data = response.json()
+            weather = {
+                "city": data["name"],
+                "temp": data["main"]["temp"],
+                "description": data["weather"][0]["description"],
+                "humidity": data["main"]["humidity"]
+            }
+        else:
+            error = "City nahi mili! Dobara try karein."
+    return render_template("index.html", weather=weather, error=error)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
